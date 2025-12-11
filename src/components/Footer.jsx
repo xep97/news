bf700782a2034b8f5168f3779b529eb0c3393674
xep97 <pettersen.egil@gmail.com>
2025-12-11 21:28:08 +0100

    visual changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        <p>This website only contains AI generated news stories as a demo</p>
+        <p>Demo website - All stories are made up AI generated</p>
     </footer>
   )
 }
